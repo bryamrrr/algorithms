80877c6007867638a3707ce46331b8d4d0cbb769
Bryam Rodriguez <brodriguezaybar@gmail.com>
2020-08-13 19:02:38 -0500

    Add brute forced solution to three sum problem

---
+++ three_sum.js
@@ -0,0 +1,28 @@
+/*
+  Given an array, find all the combinations of numbers which sum is 0
+  * All numbers in the array are unique
+*/
+
+const threeSumBruteForce = (arr) => {
+  let combinations = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      for (let k = j + 1; k < arr.length; k++) {
+        if (arr[i] + arr[j] + arr[k] === 0) {
+          combinations.push([arr[i], arr[j], arr[k]]);
+        }
+      }
+    }
+  }
+
+  return combinations;
+
+  /*
+    Brute force solution
+    Time complexity: O(n**3)
+    Space complexity: O(n)
+  */
+};
+
+console.log(threeSumBruteForce([-1, 0, 1, 2, -4, -3])); // Output = [[-1, 0,1], [1,2,-3]]
